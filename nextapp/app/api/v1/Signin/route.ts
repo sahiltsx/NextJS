c27b81508d2b614a772de6e39cf7951e83fe73d4
Sahil Sharma <sahilsharma901590@gmail.com>
2026-09-06 15:58:25 +0530

    add prisma

--- nextapp/app/api/v1/Signin/route.ts
+++ nextapp/app/api/v1/Signin/route.ts
@@ -1,11 +1,33 @@
-import { NextRequest ,NextResponse} from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-export async function POST(req:NextRequest){
-     const data=await req.json();
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
 
-     console.log(data)
+const client = new PrismaClient({ adapter });
 
-     return NextResponse.json({
-        message:"User Signin successfully"
-     })
+export async function POST(req: NextRequest) {
+  const data = await req.json();
+  const { email, password } = data;
+
+  try {
+    const user = await client.user.create({
+      data: {
+        email: email,
+        password: password
+      }
+    });
+
+    return NextResponse.json({
+      message: "User Signup successfully",
+      user
+    });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Something went wrong", error: String(error) },
+      { status: 500 }
+    );
+  }
 }
