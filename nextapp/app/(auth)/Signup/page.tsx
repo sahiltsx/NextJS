306b4cfd14aafddd427104d067a4fd9a434aa951
Sahil Sharma <sahilsharma901590@gmail.com>
2026-09-07 15:29:44 +0530

    write a new route and add prisma

---
+++ nextapp/app/(auth)/Signup/page.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import axios from "axios"
+import { useState } from "react"
+
+export default function Signin(){
+    const[email,setEmail]=useState("")
+    const [password,setPassword]=useState("")
+    return (
+        <div className="flex items-center justify-center w-screen h-screen bg-amber-50 text-neutral-500">
+            <div>
+                <input type="text " placeholder="Enter your email" onChange={e=>setEmail(e.target.value)}/>
+                <input type="password" placeholder="Enter your password" onChange={e=>setPassword(e.target.value)}/>
+                <button onClick={()=>
+                    axios.post("http://localhost:3000/api/v1/Signin",{
+                        email,
+                        password
+                    })
+                }>Sign In</button>
+            </div>
+        </div>
+    )
+}
---
+++ nextapp/app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+
+export  default function({children}:{
+    children:ReactNode
+}) {
+    return (
+        <div>
+            <div>Header</div>
+            {children}
+            <div>Footer</div>
+        </div>
+    )
+}
