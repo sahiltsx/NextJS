3d9469dd79cf5b3c16df12f18d8606292a8fdbb8
Sahil Sharma <sahilsharma901590@gmail.com>
2026-09-09 20:36:49 +0530

    initial commit

---
+++ nextapp/app/(auth)/Login/page.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react"
+
+
+export default function login(){
+    const[email,setEmail]=useState("")
+    const[password,setPassword]=useState("")
+    const[loading,setLoading]=useState(false)
+    const[error,setError]=useState<string|null>(null)
+
+    
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-50">
+           <form 
+        //    onSubmit={handleLogin}
+           className="bg-white p-8 rounded-lg shadow-md w-full max-w-sm space-y-4"
+           >
+           <h1 className="text-2xl font-semibold text-center">Login</h1>
+           <div>
+            <label className="block text-sm font-medium mb-1">Email</label>
+            <input 
+            type="email"
+            // value={email}
+            // onChange={(e)=>setEmail(e.target.value)}
+            required
+            className="w-full border rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 "
+             />
+           </div>
+        <div>
+            <label className="block text-sm font-medium mb-1">Password</label>
+            <input
+             type="password"
+            //  value={password}
+            // onChange={(e)=>setPassword(e.target.value)}
+            required
+            className="w-full border rounded-md px-3 py-2 focus:outline-none focus:ring-blue-500"
+             />
+        </div>
+        {/* {error && <p className="text-red-500 text-sm">{error</p>} */}
+
+        <button
+        type="submit"
+        // disabled={loading}
+        className="w-full bg-blue-400 text-white py-2 rounded-md hover:bg-blue-500 disabled:opacity-50"
+        >
+            {/* {loading:"Logging in....":"Login"} */}
+        </button>
+           </form>
+        </div>
+    )
+}
